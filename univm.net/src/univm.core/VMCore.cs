3bbdff94af076654f0a7312be25a9534af1ee5eb
Creeper Lv <creeperlv@users.noreply.github.com>
2024-01-07 15:52:25 +0800

    Add `RunAsm()` in `VM`

--- univm.net/src/univm.core/VMCore.cs
+++ univm.net/src/univm.core/VMCore.cs
@@ -1,97 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using univm.core.Utilities;
 
 namespace univm.core
 {
-    public sealed class VM : IDisposable
-    {
-        public MachineData machineData = new MachineData();
-        public List<VMCore> Cores = new List<VMCore>();
-        public void SetSysCall(uint Namespace, uint ID, SysCall call)
-        {
-            machineData.SetSysCall(Namespace, ID, call);
-        }
-        public void Dispose()
-        {
-            machineData.Dispose();
-        }
-        public VMCore CreateCore()
-        {
-            VMCore core = new VMCore(this);
-            Cores.Add(core);
-            return core;
-        }
-        public void DumpText(TextWriter writer, int Width = 80)
-        {
-            writer.WriteLine("Cores");
-            writer.WriteLine(Cores.Count);
-            int _w = 0;
-            int ID = 0;
-            foreach (var item in Cores)
-            {
-                writer.WriteLine($"Core {ID}");
-                writer.WriteLine(item.coreData.RegisterData.Length);
-                for (int i = 0; i < item.coreData.RegisterData.Length; i++)
-                {
-                    writer.Write(item.coreData.RegisterData[i].ToHEX());
-                    _w += 2;
-                    if (_w >= Width)
-                    {
-                        writer.WriteLine();
-                        _w = 0;
-                    }
-                }
-                writer.WriteLine();
-                ID++;
-            }
-            writer.WriteLine("Memory");
-            writer.WriteLine(machineData.MemBlocks.Count);
-            unsafe
-            {
-
-                foreach (var item in machineData.MemBlocks)
-                {
-                    _w = 0;
-                    writer.WriteLine(item.Size);
-                    for (int i = 0; i < item.Size; i++)
-                    {
-                        writer.Write((item.Data[i]).ToHEX());
-                        _w += 2;
-                        if (_w >= Width)
-                        {
-                            writer.WriteLine();
-                            _w = 0;
-                        }
-                    }
-                    writer.WriteLine();
-                }
-            }
-        }
-        public void DumpBinary(Stream output)
-        {
-            output.WriteData(Cores.Count);
-            foreach (var item in Cores)
-            {
-                output.WriteData(item.coreData.RegisterData.Length);
-                output.Write(item.coreData.RegisterData);
-
-            }
-            output.WriteData(machineData.MemBlocks.Count);
-            unsafe
-            {
-                foreach (var item in machineData.MemBlocks)
-                {
-                    output.WriteData(item.Size);
-                    for (int i = 0; i < item.Size; i++)
-                    {
-                        output.WriteByte(item.Data[i]);
-                    }
-                }
-            }
-        }
-    }
     public sealed class VMCore : IDisposable
     {
         public CoreData coreData = new CoreData();
@@ -204,7 +114,7 @@ namespace univm.core
                 coreData.CallStack[coreData.CallStack.Count - 1] = (frame);
                 Execute(inst);
                 frame = coreData.CallStack[coreData.CallStack.Count - 1];
-                if (machineData.assemblies[(int)frame.AssemblyID].Instructions.Length >= frame.PCInAssembly)
+                if (machineData.assemblies[(int)frame.AssemblyID].Instructions.Length <= frame.PCInAssembly)
                 {
                     return;
                 }
--- univm.net/src/univm/Program.cs
+++ univm.net/src/univm/Program.cs
@@ -8,37 +8,44 @@ class Program
     static void Main(string[] args)
     {
         VM vm = new VM();
-        MachineData machine = vm.machineData;
-        VMCore vMCore=vm.CreateCore();
-        CoreData coredata = vMCore.coreData;
-        DefaultSysCalls.SetupSysCall(vm);
-        coredata.SetDataToRegister(0, 123);
-        Console.WriteLine(coredata.GetDataFromRegister<int>(0));
-        coredata.SetDataToRegister(1, 123);
-        Console.WriteLine(coredata.GetDataFromRegister<int>(0));
-        coredata.SetDataToRegister(0, 1.025f);
-        Console.WriteLine(coredata.GetDataFromRegister<float>(0));
-        var ID = machine.Alloc(4, coredata);
-        var ID2 = machine.Alloc(4, coredata);
-        MemPtr L = new MemPtr() { MemID = ID, Offset = 0 };
-        MemPtr R = new MemPtr() { MemID = ID2, Offset = 0 };
-        unsafe
+        //MachineData machine = vm.machineData;
+        //VMCore vMCore = vm.CreateCore();
+        //CoreData coredata = vMCore.coreData;
+        //DefaultSysCalls.SetupSysCall(vm);
+        //coredata.SetDataToRegister(0, 123);
+        //Console.WriteLine(coredata.GetDataFromRegister<int>(0));
+        //coredata.SetDataToRegister(1, 123);
+        //Console.WriteLine(coredata.GetDataFromRegister<int>(0));
+        //coredata.SetDataToRegister(0, 1.025f);
+        //Console.WriteLine(coredata.GetDataFromRegister<float>(0));
+        //var ID = machine.Alloc(4, coredata);
+        //var ID2 = machine.Alloc(4, coredata);
+        //MemPtr L = new MemPtr() { MemID = ID, Offset = 0 };
+        //MemPtr R = new MemPtr() { MemID = ID2, Offset = 0 };
+        
+        UniVMAssembly asm = new UniVMAssembly()
         {
-            Console.WriteLine("Sizeof(MemPtr):" + sizeof(MemPtr));
-        }
-        coredata.SetDataToRegister(RegisterDefinition.RegisterOffset_00, L);
-        coredata.SetDataToRegister(RegisterDefinition.RegisterOffset_01, R);
-        machine.SetDataToMemPtr(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00), 1.234f, coredata);
-        machine.MemCpy(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00),
-                           coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_01),
-                           4, coredata);
-        Console.WriteLine(machine.GetDataFromMemPtr<float>(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00), coredata));
-        Console.WriteLine(machine.GetDataFromMemPtr<float>(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_01), coredata));
-        machine.Realloc((int)ID, 8, coredata);
-        Console.WriteLine(machine.GetDataFromMemPtr<float>(L, coredata));
-        L.Offset += 4;
-        machine.SetDataToMemPtr(L, 117, coredata    );
-        Console.WriteLine(machine.GetDataFromMemPtr<int>(L, coredata));
+            Instructions = [
+                new Inst() { Op_Code = InstOPCodes.BASE_SET32, Data0 = RegisterDefinition.RegisterOffset_01, Data1 = 0x1000, Data2 = 0 },
+                new Inst() { Op_Code = InstOPCodes.BASE_SET32, Data0 = RegisterDefinition.RegisterOffset_02, Data1 = 0x1234, Data2 = 0 },
+                new Inst() { Op_Code = InstOPCodes.BASE_ADD, Data0 = RegisterDefinition.RegisterOffset_00, 
+                Data1 = RegisterDefinition.RegisterOffset_01, Data2 = RegisterDefinition.RegisterOffset_02},
+                ],
+        };
+        //coredata.SetDataToRegister(RegisterDefinition.RegisterOffset_00, L);
+        //coredata.SetDataToRegister(RegisterDefinition.RegisterOffset_01, R);
+        //machine.SetDataToMemPtr(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00), 1.234f, coredata);
+        //machine.MemCpy(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00),
+        //                   coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_01),
+        //                   4, coredata);
+        //Console.WriteLine(machine.GetDataFromMemPtr<float>(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_00), coredata));
+        //Console.WriteLine(machine.GetDataFromMemPtr<float>(coredata.GetDataFromRegister<MemPtr>(RegisterDefinition.RegisterOffset_01), coredata));
+        //machine.Realloc((int)ID, 8, coredata);
+        //Console.WriteLine(machine.GetDataFromMemPtr<float>(L, coredata));
+        //L.Offset += 4;
+        //machine.SetDataToMemPtr(L, 117, coredata);
+        //Console.WriteLine(machine.GetDataFromMemPtr<int>(L, coredata));
+        vm.RunAsm(asm);
         using FileStream stream = File.OpenWrite("coredump");
         vm.DumpBinary(stream);
         stream.Flush();
